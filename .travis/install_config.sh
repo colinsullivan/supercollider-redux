ef533c642388225fb68d46e8bc5ab85343fd3529
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 01:36:04 -0800

    travis

--- .travis/install_config.sh
+++ .travis/install_config.sh
@@ -10,9 +10,4 @@
 #cat $TRAVIS_BUILD_DIR/.supercollider.yaml
 
 echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/supercollider-redux\");" >> $TRAVIS_BUILD_DIR/install_quark.sc
-
-echo "install_quark.sc"
-cat $TRAVIS_BUILD_DIR/install_quark.sc
-
-sclang $TRAVIS_BUILD_DIR/install_quark.sc
-
+echo "0.exit();" >> $TRAVIS_BUILD_DIR/install_quark.sc
--- .travis/install_supercollider.sh
+++ .travis/install_supercollider.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
-npm install -g lintspaces-cli
-sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-sudo add-apt-repository --yes ppa:beineri/opt-qt591-trusty
-sudo apt-get update
-sudo apt-get install --yes build-essential gcc-4.9 g++-4.9 cmake pkg-config libjack-jackd2-dev libsndfile1-dev libasound2-dev libavahi-client-dev libreadline6-dev libfftw3-dev libicu-dev libxt-dev libudev-dev
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-sudo update-alternatives --auto gcc
-git clone --recursive https://github.com/supercollider/supercollider.git
-mkdir supercollider/build && cd supercollider/build
-cmake -DSC_QT=OFF -DCMAKE_BUILD_TYPE=Release ..
+
+# Need to build SC without GUI
+cd $TRAVIS_BUILD_DIR/supercollider
+mkdir build && cd build
+cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/supercollider
 make
 sudo make install
 
+# Builds sc3-plugins
+cd $TRAVIS_BUILD_DIR/sc3-plugins
+mkdir build && cd build
+cmake -DSC_PATH=$TRAVIS_BUILD_DIR/supercollider -DCMAKE_BUILD_TYPE=Release ..
+make
+sudo make install
