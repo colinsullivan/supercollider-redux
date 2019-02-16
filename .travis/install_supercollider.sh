a51279dc31b4b3e6a865cb5fbf4829ddb9235419
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 11:46:12 -0800

    travis

--- .travis/install_supercollider.sh
+++ .travis/install_supercollider.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
+# Clone supercollider and sc3-plugins if not in cache
+if [[ ! -d $HOME/supercollider ]]; then
+  git clone --recursive https://github.com/supercollider/supercollider.git $HOME/supercollider
+  cd $HOME/supercollider
+  git checkout Version-3.10.2
+fi
+if [[ ! -d $HOME/sc3-plugins ]]; then
+  git clone --recursive https://github.com/supercollider/sc3-plugins.git $HOME/sc3-plugins
+  cd $HOME/sc3-plugins
+  git checkout Version-3.10.0
+fi
+
 # Need to build SC without GUI
-mkdir -p $HOME/supercollider-build && cd $HOME/supercollider-build
-cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/supercollider
+cd $HOME/supercollider
+mkdir -p build && cd build
+cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $HOME/supercollider
 make
 sudo make install
 
 # Builds sc3-plugins
-mkdir -p $HOME/sc3-plugins-build && cd $HOME/sc3-plugins-build
-cmake -DSC_PATH=$TRAVIS_BUILD_DIR/supercollider -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/sc3-plugins
+cd $HOME/sc3-plugins
+mkdir -p build && cd build
+cmake -DSC_PATH=$HOME/supercollider -DCMAKE_BUILD_TYPE=Release $HOME/sc3-plugins
 make
 sudo make install
