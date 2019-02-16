6e0903918c497e7156dfe218f67bea70e0355893
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 01:03:24 -0800

    travis

--- .travis/install_config.sh
+++ .travis/install_config.sh
@@ -3,6 +3,8 @@
 #cp $TRAVIS_BUILD_DIR/.travis/.supercollider.yaml $TRAVIS_BUILD_DIR/.supercollider.yaml
 #cp .travis/sclang_conf.yaml ./sclang_conf.yaml
 #echo "sclang_conf: $TRAVIS_BUILD_DIR/sclang_conf.yaml" >> $TRAVIS_BUILD_DIR/.supercollider.yaml
+#echo "sclang_conf.yaml:"
+#cat sclang_conf.yaml
 
 #echo ".supercollider.yaml:"
 #cat $TRAVIS_BUILD_DIR/.supercollider.yaml
@@ -12,5 +14,5 @@ echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/supercollider-redux\");" >> $TRA
 echo "install_quark.sc"
 cat $TRAVIS_BUILD_DIR/install_quark.sc
 
-#echo "sclang_conf.yaml:"
-#cat sclang_conf.yaml
+sclang $TRAVIS_BUILD_DIR/install_quark.sc
+
