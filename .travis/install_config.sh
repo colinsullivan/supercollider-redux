7a4f4bac757088be0bc3dbc638538c830ef91222
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 02:15:57 -0800

    travis

--- .travis/install_config.sh
+++ .travis/install_config.sh
@@ -10,4 +10,5 @@
 #cat $TRAVIS_BUILD_DIR/.supercollider.yaml
 
 echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/supercollider-redux\");" >> $TRAVIS_BUILD_DIR/install_quark.sc
+echo "thisProcess.shutdown();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 echo "0.exit();" >> $TRAVIS_BUILD_DIR/install_quark.sc
