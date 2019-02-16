e77563ec3a021427d34994d3a65f80793625425a
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 09:40:27 -0800

    travis

--- .travis/install_supercollider.sh
+++ .travis/install_supercollider.sh
@@ -2,12 +2,12 @@
 
 # Need to build SC without GUI
 mkdir -p $HOME/supercollider-build && cd $HOME/supercollider-build
-cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $HOME/supercollider
+cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/supercollider
 make
 sudo make install
 
 # Builds sc3-plugins
 mkdir -p $HOME/sc3-plugins-build && cd $HOME/sc3-plugins-build
-cmake -DSC_PATH=$HOME/supercollider -DCMAKE_BUILD_TYPE=Release $HOME/sc3-plugins
+cmake -DSC_PATH=$TRAVIS_BUILD_DIR/supercollider -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/sc3-plugins
 make
 sudo make install
