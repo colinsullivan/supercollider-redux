9d7332607b684e652ccce1b9ec151db5a241275d
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 09:38:41 -0800

    travis

--- .travis/install_config.sh
+++ .travis/install_config.sh
@@ -13,3 +13,4 @@ echo "Quarks.install(\"$TRAVIS_BUILD_DIR/quarks/supercollider-redux\");" >> $TRA
 echo "thisProcess.shutdown();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 echo "0.exit();" >> $TRAVIS_BUILD_DIR/install_quark.sc
 sclang $TRAVIS_BUILD_DIR/install_quark.sc
+exit 0
--- .travis/install_supercollider.sh
+++ .travis/install_supercollider.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Need to build SC without GUI
-mkdir -p $TRAVIS_BUILD_DIR/supercollider-build && cd $TRAVIS_BUILD_DIR/supercollider-build
-cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/supercollider
+mkdir -p $HOME/supercollider-build && cd $HOME/supercollider-build
+cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_BUILD_TYPE=Release $HOME/supercollider
 make
 sudo make install
 
 # Builds sc3-plugins
-mkdir -p $TRAVIS_BUILD_DIR/sc3-plugins-build && cd $TRAVIS_BUILD_DIR/sc3-plugins-build
-cmake -DSC_PATH=$TRAVIS_BUILD_DIR/supercollider -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR/sc3-plugins
+mkdir -p $HOME/sc3-plugins-build && cd $HOME/sc3-plugins-build
+cmake -DSC_PATH=$HOME/supercollider -DCMAKE_BUILD_TYPE=Release $HOME/sc3-plugins
 make
 sudo make install
