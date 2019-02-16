bf07f6706f911839695dd8c7ddb13b9bee116a20
Colin Sullivan <colin@colin-sullivan.net>
2019-02-16 01:13:57 -0800

    travis

--- .travis/install_supercollider.sh
+++ .travis/install_supercollider.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-sudo apt-get install -y build-essential libsndfile1-dev libasound2-dev libavahi-client-dev libicu-dev libreadline6-dev libfftw3-dev libxt-dev libudev-dev pkg-config git cmake qt5-default qt5-qmake qttools5-dev qttools5-dev-tools qtdeclarative5-dev qtpositioning5-dev libqt5sensors5-dev libqt5opengl5-dev qtwebengine5-dev libqt5svg5-dev libqt5websockets5-dev libjack-jackd2-dev
+npm install -g lintspaces-cli
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository --yes ppa:beineri/opt-qt591-trusty
+sudo apt-get update
+sudo apt-get install --yes build-essential gcc-4.9 g++-4.9 cmake pkg-config libjack-jackd2-dev libsndfile1-dev libasound2-dev libavahi-client-dev libreadline6-dev libfftw3-dev libicu-dev libxt-dev libudev-dev
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 --slave /usr/bin/g++ g++ /usr/bin/g++-4.9
+sudo update-alternatives --auto gcc
 git clone --recursive https://github.com/supercollider/supercollider.git
 mkdir supercollider/build && cd supercollider/build
 cmake -DSC_QT=OFF -DCMAKE_BUILD_TYPE=Release ..
