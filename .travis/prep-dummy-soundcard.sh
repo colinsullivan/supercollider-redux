3bfd64bff81668663beb2150cf679d5251f5dfca
Colin Sullivan <colin@colin-sullivan.net>
2019-11-16 16:28:50 +0000

    sudo

--- .travis/prep-dummy-soundcard.sh
+++ .travis/prep-dummy-soundcard.sh
@@ -1,4 +1,4 @@
-sudo cat << EOF > /home/travis/.asoundrc
+cat << EOF > /home/travis/.asoundrc
        pcm.dummy {
           type hw
           card 0
@@ -9,8 +9,8 @@ sudo cat << EOF > /home/travis/.asoundrc
           card 0
        }
 EOF
-sudo chmod go+r /home/travis/.asoundrc
-sudo cat << EOF >> /etc/modules.conf
+chmod go+r /home/travis/.asoundrc
+cat << EOF >> /etc/modules.conf
 # OSS/Free portion - card #1
 alias sound-slot-0 snd-card-0
 alias sound-service-0-0 snd-mixer-oss
@@ -19,8 +19,6 @@ alias sound-service-0-3 snd-pcm-oss
 alias sound-service-0-8 snd-seq-oss
 alias sound-service-0-12 snd-pcm-oss
 EOF
-sudo modprobe snd-dummy 
+modprobe snd-dummy 
 # ; modprobe snd-pcm-oss ; modprobe snd-mixer-oss ; modprobe snd-seq-oss
-sudo mkdir -p tmp && chmod 777 tmp
-
-jackd
+mkdir -p tmp && chmod 777 tmp
