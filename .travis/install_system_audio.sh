0a88f856c5e6c3d7582363e43355852156392dff
Colin Sullivan <colin@colin-sullivan.net>
2019-11-16 16:14:19 +0000

    installing jack would help

--- .travis/install_system_audio.sh
+++ .travis/install_system_audio.sh
@@ -4,3 +4,4 @@ sudo apt-get update -qq
 sudo usermod -a -G audio travis
 sudo apt-get install -y portaudio19-dev
 sudo apt-get install -y libasound2-dev alsa-utils alsa-oss
+sudo apt-get install -y jack-tools
